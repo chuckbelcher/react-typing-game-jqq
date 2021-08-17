1bdba064e9021bc43c6e9f55bc1993b941873368
Chuck Belcher <cfbelcher@pateogroup.com>
2021-08-16 23:08:32 -0400

    Added styled components to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { StyledNavbar, StyledNavBrand, StyledNavItems, StyledLink } from '../styled/Navbar'
+import { Accent } from '../styled/Misc'
 
 export default function Navbar() {
     return (
-        <nav>
-            <div>
-                <Link to="/">Learn.Build.<span>Type.</span></Link>
-            </div>
-            <ul>
-                <li><Link to="/highscores">High Scores</Link></li>
-                <li><Link to="/">Login</Link></li>
-            </ul>
-        </nav>
+
+        <StyledNavbar>
+            <StyledNavBrand>
+                <Link to="/">Learn.Build.<Accent>Type</Accent>.</Link>
+            </StyledNavBrand>
+            <StyledNavItems>
+                <li><StyledLink to="/highscores">High Scores</StyledLink></li>
+                <li><StyledLink to="/">Login</StyledLink></li>
+            </StyledNavItems>
+        </StyledNavbar>
     )
 }
