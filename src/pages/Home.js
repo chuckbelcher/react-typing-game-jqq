edc2f2d88fc5ab3e2e80fb8f01fa8ca3d3165c34
Chuck Belcher <cfbelcher@pateogroup.com>
2021-08-17 17:38:03 -0400

    Created styled components for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import CTA from '../styled/CTA';
+import { Accent, StyledTitle } from '../styled/Misc'
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
-        </div>
+            <StyledTitle>Ready to <Accent>type</Accent>?</StyledTitle>
+            <CTA to="/game">
+                Click or type '<Accent>'s'</Accent>' to start playing!
+            </CTA>
+        </div >
     )
 }
