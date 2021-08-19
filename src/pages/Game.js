0cb7d245d09e6b885bca93d8e17f7d4ece154731
Chuck Belcher <cfbelcher@pateogroup.com>
2021-08-18 23:10:22 -0400

    added functionality to compare key stroke with currentKey and update score

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { StyledCharacter, StyledGame, StyledScore, StyledTimer } from '../styled/Game';
 import { Strong } from '../styled/Misc';
 export default function Game({ history }) {
 
     const MAX_SECONDS = 15;
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const [currentCharacter, setCurrentCharacter] = useState('');
     const [score, setScore] = useState(0);
     const [ms, setMs] = useState(0);
     const [seconds, setSeconds] = useState(MAX_SECONDS);
 
     useEffect(() => {
+        setRandomCharacter();
         const currentTime = new Date();
         const interval = setInterval(() => updateTime(currentTime), 1);
         return () => clearInterval(interval);
@@ -17,20 +20,35 @@ export default function Game({ history }) {
 
     useEffect(() => {
         if (seconds < -1) {
+            //Todo: Save the score
             history.push('/gameOver');
         }
     }, [seconds, ms, history]);
 
-    const keyUpHandler = (e) => {
-        console.log(e.key);
-    }
+    const keyUpHandler = useCallback((e) => {
+
+        if (e.key === currentCharacter) {
+            setScore((prevScore) => prevScore + 1)
+        } else {
+            if (score > 0) {
+                setScore((prevScore => prevScore - 1))
+            }
+        }
+        setRandomCharacter();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentCharacter])
 
     useEffect(() => {
         document.addEventListener('keyup', keyUpHandler);
         return () => {
             document.removeEventListener('keyup', keyUpHandler)
         }
-    }, []);
+    }, [keyUpHandler]);
+
+    const setRandomCharacter = () => {
+        const randomInt = Math.floor(Math.random() * 36);
+        setCurrentCharacter(characters[randomInt]);
+    }
 
     const updateTime = (startTime) => {
         const endTime = new Date();
@@ -54,7 +72,7 @@ export default function Game({ history }) {
     return (
         <StyledGame>
             <StyledScore>Score: <Strong>{score}</Strong></StyledScore>
-            <StyledCharacter>X</StyledCharacter>
+            <StyledCharacter>{currentCharacter}</StyledCharacter>
             <StyledTimer>Time: <Strong>{seconds}:{ms}</Strong></StyledTimer>
         </StyledGame>
     )
