663ff09e556375d8eb88f2a4a3acac1b586242e6
Chuck Belcher <cfbelcher@pateogroup.com>
2021-08-26 00:06:59 -0400

    added scores from airtable to the Highscores page

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { ScoreLI, ScoresList } from '../styled/HighScores'
 
 export default function HighScores() {
+    const [highScores, setHighScores] = useState([])
+
+    useEffect(() => {
+        const loadHighScores = async () => {
+            try {
+                const res = await fetch('/.netlify/functions/getHighScores')
+                const scores = await res.json();
+                setHighScores(scores)
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        loadHighScores();
+    },[]);
+
     return (
         <div>
             <h1>HighScores</h1>
+            <ScoresList>
+                {highScores.map((score) => 
+                    <ScoreLI key={score.id}>{score.fields.name} ... {score.fields.score}</ScoreLI>
+                )}
+            </ScoresList>
         </div>
     )
 }
---
+++ src/styled/HighScores.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ScoresList = styled.ol`
+    padding-left: 10;
+`
+
+export const ScoreLI = styled.li`
+    font-size: 1.6rem;
+    margin-bottom: .5rem;
+`
